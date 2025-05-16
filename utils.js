4f5b2cc2f29e37ab94390ca9ef64a444f9fea30e
testmail-ihub <58020879+testmail-ihub@users.noreply.github.com>
2025-05-16 17:49:52 +0530

    Added utils.js file

---
+++ utils.js
@@ -0,0 +1,45 @@
+export function debounce(func, delay) {
+    let timeout;
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), delay);
+    };
+}
+
+export function throttle(func, limit) {
+    let lastFunc;
+    let lastRan;
+    return function (...args) {
+        const context = this;
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function () {
+                if (Date.now() - lastRan >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+}
+
+export function deepClone(obj) {
+    if (typeof obj !== 'object' || obj === null) return obj;
+    if (Array.isArray(obj)) {
+        return obj.map(deepClone);
+    }
+    const clone = {};
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            clone[key] = deepClone(obj[key]);
+        }
+    }
+    return clone;
+}
+
+export function isEmptyObject(obj) {
+    return Object.keys(obj).length === 0 && Object.getPrototypeOf(obj) === Object.prototype;
+}
